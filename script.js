f743f4e28f88fb93277f0973b435d460a9b8e42f
‘Talah <talahh3@gmail.com>
2022-08-03 19:20:43 -0400

    JS additions Lab 05

---
+++ script.js
@@ -0,0 +1,14 @@
+let drink = prompt("What is your favorite drink currently?")
+console.log(drink)
+if (drink ==="water") {
+    alert ("water is the best")
+} 
+
+else if (response = " "); {
+    alert ("Welcome!")
+};
+
+const usersName = prompt ("What is your name?");
+console.log(usersName);
+
+console.log("Hi " + usersName + "!");
