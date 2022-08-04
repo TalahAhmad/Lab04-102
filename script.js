3cae804799fb555ac6187562e3b50cc0c1f5bdf7
‘Talah <talahh3@gmail.com>
2022-08-04 15:29:49 -0400

    added function

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-let drink = prompt("What is your favorite drink currently?")
-console.log(drink)
-if (drink ==="water") {
-    alert ("water is the best")
-} 
+// let drink = prompt("What is your favorite drink currently?")
+// console.log(drink)
+// if (drink ==="water") {
+//     alert ("water is the best")
+// } 
 
-else if (response = " "); {
-    alert ("Welcome!")
+// else if (response = " "); {
+//     alert ("Welcome!")
+// };
+
+// const usersName = prompt ("What is your name?");
+// console.log(usersName);
+
+// console.log("Hi " + usersName + "!");
+
+function usersDrink(){
+    const drink = prompt("What is your favorite drink?")
+console.log(drink)
+if (drink ==="water") 
+    alert ("water is the best")
+}
+
+usersDrink()
+
+function usersDifferentResponse(){
+    if (response = " "); 
+        alert ("Welcome!")
 };
 
-const usersName = prompt ("What is your name?");
+usersDifferentResponse()
+
+function usersName(){
+    const usersName = prompt ("What is your name?");
 console.log(usersName);
 
 console.log("Hi " + usersName + "!");
+}
 
